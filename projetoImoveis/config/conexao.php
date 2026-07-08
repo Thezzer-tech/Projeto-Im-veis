c98ada379274b8609d740649d37f222c44aac6ec
Thezzer <miguelarcanjocavallimedeiros@gmail.com>
2026-07-08 20:16:47 -0300

    alteração da conexão para conseguir conectar ao mysql alocado no aiven

--- projetoImoveis/config/conexao.php
+++ projetoImoveis/config/conexao.php
@@ -1,5 +1,6 @@
 <?php
 
+    /*
     $dominio = "mysql:host=localhost;dbname=projetoImoveis;charset=utf8"; //localhost significa que o banco de dados esta rodando no próprio computador 
     $usuario = "root"; //nome padrão de todo banco de dados local
     $senha = "";
@@ -15,4 +16,35 @@
         die("Erro ao conectar ao banco de dados:".$e->getMessage());
     }
 
-    ?>
+    rodar em máquina local
+    */
+
+    // Em vez de colocar a senha direto aqui, o PHP vai puxar da nuvem
+    $host = getenv('DB_HOST');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+    $dbname = getenv('DB_NAME');
+    $port = getenv('DB_PORT');
+
+    // O PDO exige a montagem de um DSN (Data Source Name)
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
+    // Configurações extras para o PDO (opcional, mas muito recomendado)
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Força erros a caírem no Catch
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Retorna dados como array associativo
+        PDO::ATTR_EMULATE_PREPARES   => false,                  // Mais segurança contra SQL Injection
+    ];
+
+
+    try{
+
+        $pdo = new PDO($dsn, $user, $pass, $options);
+
+    }catch (PDOException $e) {
+
+        die("Falha na conexão com o banco de dados: " .$e->getMessage());
+
+    }
+   
+?>
